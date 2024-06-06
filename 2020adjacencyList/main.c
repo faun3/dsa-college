f8b3fea369697680fa714ec7d8a3c2f205c6ef1a
aan <aaa>
2024-06-06 22:28:07 +0300

    start work on first graph subject

---
+++ 2020adjacencyList/main.c
@@ -0,0 +1,11 @@
+// https://github.com/snaake20/ase_info_an_2_sem_2/blob/main/SDD/exercitii_examen/host%20-%20LA%202xV%20-%201/Host.pdf
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main() {
+
+}
